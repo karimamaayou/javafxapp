97a263b545f2888858ce8b78c2418f6c57eb546b
Karimo21 <karimamaayou20@gmail.com>
2024-12-21 13:28:10 +0100

    connected the database

--- serviceapp/src/application/Main.java
+++ serviceapp/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.sql.Connection;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,6 +12,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
+			
+			 Connection connection = MysqlConnection.getDBConnection();
+			 if(connection != null) {
+				 
+				 System.out.println("Connected");
+			 }
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
