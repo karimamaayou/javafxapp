5bf788e4238e114164f5bfa44f39882531d89886
Ilyas122ux <halima1965ilyass@gmail.com>
2024-12-23 13:11:39 +0100

    have added the client and prestataire view and controller

---
+++ serviceapp/src/application/AddClientController.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddClientController {
+	  @FXML
+	    private Label addressErrorLabel;
+
+	    @FXML
+	    private Label emailErrorLabel;
+
+	    @FXML
+	    private TextField emailField;
+
+	    @FXML
+	    private TextField firstNameField;
+
+	    @FXML
+	    private TextField lastNameField;
+
+	    @FXML
+	    private Label nameErrorLabel;
+
+	    @FXML
+	    private Label phoneErrorLabel;
+
+	    @FXML
+	    private TextField phoneField;
+
+	    @FXML
+	    private ChoiceBox<?> villeField;
+
+}
---
+++ serviceapp/src/application/AddPrestataireController.java
@@ -0,0 +1,49 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddPrestataireController {
+    @FXML
+    private Label addressErrorLabel;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private Label emailErrorLabel;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField maxPriceField;
+
+    @FXML
+    private TextField minPriceField;
+
+    @FXML
+    private Label nameErrorLabel;
+
+    @FXML
+    private Label phoneErrorLabel;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private Label priceErrorLabel;
+
+    @FXML
+    private Label titleErrorLabel;
+
+    @FXML
+    private TextField titleField;
+}
