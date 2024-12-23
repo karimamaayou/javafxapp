4cbe56de61373b1eeecbaf6458acb2dd7864da5c
Karimo21 <karimamaayou20@gmail.com>
2024-12-23 20:13:58 +0100

    ..

--- serviceapp/src/application/ClientController.java
+++ serviceapp/src/application/ClientController.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -31,6 +32,11 @@ public class ClientController {
 
     @FXML
     private TableColumn<Client, String> villeColumn;
+    
+    @FXML
+    void ajouterClient(ActionEvent event) {
+
+    }
     
     
     
